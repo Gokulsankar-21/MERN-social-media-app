6108383ca6e372991f9724836d2ba2da934d84cc
GokulDev <gokulsankar@gamil.com>
2024-03-28 09:01:43 +0530

    created the routes for the apps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { LoginPage } from "./scenes/Login/LoginPage";
+import { HomePage } from "./scenes/Home/HomePage";
+import { ProfilePage } from "./scenes/Profle/ProfilePage";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        {/** ithu / na nama login page ku kondu porom */}
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
